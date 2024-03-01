dd4e700fe7a7c6b3d488b7f72b3757634b66f094
Sage Kolpin <sage@Sages-MacBook-Air.local>
2024-02-29 17:06:37 -0800

    adding navbar, and services section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
   return (
 
    <>
-<nav class="navbar sticky-top navbar-light bg-light">
-<h1 id="pcpc1">Pacific Counseling and Pyschological Center</h1>
-</nav>
-
+<Navbar bg="dark" data-bs-theme="dark" sticky="top">
+        <Container>
+        <h1 id="pcpc1">Pacific Counseling and Pyschological Center</h1>
+        <Button variant="outline-success">Contact Us</Button>
+        </Container>
+      </Navbar>
+      
 <div class='about1'>
   <h2 id="empower">Empowering You to Thrive</h2>
 </div>
 
 <div class="about2">
   <h2>Our Services </h2>
-  <h4>Indiduial Counseling</h4>
-  <h4>Family Therapy</h4>
-  <h4>Group Sessions</h4>
-  <h4>Child Thearpy</h4>
-  <h4>Research</h4>
+  <div id="each">
+  <div id="one">
+    <h4 id="words1">Individual Counseling</h4>
+  </div>
+  <div id="two">
+  <h4 id="words2">Family Therapy</h4>
+  </div>
+  <div id="four">
+   <h4 id="words4">Child Therapy</h4></div>
+   <div id="three">
+  <h4 id="words3">Group Sessions</h4>
+  </div>
+  </div>
 </div>
+
+<h2>Book an Appointment Now</h2>
+
+
+<h2>The Doctor is In</h2>
    </>
   );
 }
